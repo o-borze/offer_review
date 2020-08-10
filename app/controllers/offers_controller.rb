6582a32cbd6fe0b9c0ce309c5f9e9219e3d51d69
o-borze <borzemska@gmail.com>
2020-08-10 16:51:04 +0200

    Modify permitted params

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,18 +1,18 @@
 class OffersController < ApplicationController
   def index
     @offers = Offer.all
-    render json: @projects, include: :attributes, status: :ok
+    render json: @projects, include: :products, status: :ok
   end
   
   def show
     @offer = Offer.find(params[:id])
-    render json: @offer, include: :attributes, status: :ok
+    render json: @offer, include: :products, status: :ok
   end
   
   def create
     @offer = Offer.new(offer_params)
     if @offer.save
-      render json: @offer, include: :attributes, status: :created
+      render json: @offer, include: :products, status: :created
     else
       render nothing: true, status: :unprocessable_entity
     end
@@ -20,7 +20,7 @@ class OffersController < ApplicationController
   
   def update
     @offer = Offer.find(params[:id])
-    if @offer.update(offer_params)
+    if @offer.update(offer_params.dig(:offer, :state))
       render json: @offer, status: :ok
     else
       render nothing: true, status: :unprocessable_entity
@@ -29,6 +29,6 @@ class OffersController < ApplicationController
   end
   
   def offer_params
-    params.require(:offer).permit()
+    params.require(:offer).permit(:state, products: [:quantity, :type, product_attributes: [:key, :value, :type]])
   end
 end
